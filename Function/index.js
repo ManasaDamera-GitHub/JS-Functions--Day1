7b80c4b4d93f331f629288879c5b76e287edb3c0
DameraManasa-GitHub <dameramanasa25@gmail.com>
2024-12-09 22:01:48 +0530

    Function definition and function calling

---
+++ Function/index.js
@@ -0,0 +1,37 @@
+// Question 1
+function greet() {
+    console.log("Hello!");
+}
+greet();
+
+// Question 2
+function showMessage() {
+    console.log("This is a message!");
+}
+showMessage();
+
+// Question 3
+function logInfo() {
+    console.log("Logging Information...");
+}
+logInfo();
+
+// Question 4
+function startGame() {
+    console.log("Game Started!");
+    endGame();
+}
+function endGame() {
+    console.log("Game Over!");
+}
+startGame();
+
+// // Question 5
+function beginTask() {
+    console.log("Task Started");
+    finishTask();
+}
+function finishTask() {
+    console.log("Task Finished");
+}
+beginTask();
